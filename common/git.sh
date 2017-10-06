33af810b9af54539fd1a07574e558f9e469c970c
Michael P Olson <michael.olson@netcommwireless.com>
2017-10-06 19:01:00 -0400

    Updating my gitconfig and bashrc and git scripts

--- common/git.sh
+++ common/git.sh
@@ -1,23 +1,24 @@
 # git aliases
-alias ga='git add'
-alias gb='git branch'
-alias gl='git pull'
-alias gp='git push'
-alias gd='git diff'
-alias gdc='git diff --cached'
-alias gs='git status'
-alias gss='git stash save'
-alias gsp='git stash pop'
+alias gadd='git add'
+alias gbr='git branch'
+alias gpull='git pull'
+alias gpush='git push'
+alias gdiff='git diff'
+alias gdiffc='git diff --cached'
+alias gst='git status'
+alias gsave='git stash save'
+alias gpop='git stash pop'
 alias gmv='git mv'
 alias grm='git rm'
-alias grn='git-rename'
+alias grename='git-rename'
 alias glog="git log"
+alias ghide="git update-index --assume-unchanged"
+alias gunhide="git update-index --no-assume-unchanged"
+alias gunhideall="git-unhide-all"
+alias gundolast='git reset --soft HEAD~1'
+alias gcount='git shortlog -sn'
+#alias cpbr="git rev-parse --abbrev-ref HEAD | pbcopy"
 
-# alias git-amend='git commit --amend -C HEAD'
-alias git-undo='git reset --soft HEAD~1'
-alias git-count='git shortlog -sn'
-alias git-undopush="git push -f origin HEAD^:master"
-alias cpbr="git rev-parse --abbrev-ref HEAD | pbcopy"
 # git root
 alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
 
@@ -25,6 +26,10 @@ alias sub-pull='git submodule foreach git pull origin master'
 
 give-credit() {
     git commit --amend --author $1 <$2> -C HEAD
+}
+
+git-unhide-all() {
+    git ls-files -v | grep '^h' | sed 's/^..//' | sed 's/\ /\\ /g' | xargs -I FILE git update-index --no-assume-unchanged FILE || true
 }
 
 # a simple git rename file function
