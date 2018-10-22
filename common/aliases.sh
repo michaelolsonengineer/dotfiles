6cf7a286b96f4abe7ee753830a44d8042b635863
Michael P Olson <michael.olson@netcommwireless.com>
2018-10-22 15:45:04 -0400

    Adding some nmap aliases and adding a websearch plugin for easy commandline searches.

--- common/aliases.sh
+++ common/aliases.sh
@@ -299,6 +299,38 @@ netinfo ()
 	echo "---------------------------------------------------"
 }
 
+# Nmap options are:
+#  -sS - TCP SYN scan
+#  -v - verbose
+#  -T1 - timing of scan. Options are paranoid (0), sneaky (1), polite (2), normal (3), aggressive (4), and insane (5)
+#  -sF - FIN scan (can sneak through non-stateful firewalls)
+#  -PE - ICMP echo discovery probe
+#  -PP - timestamp discovery probe
+#  -PY - SCTP init ping
+#  -g - use given number as source port
+#  -A - enable OS detection, version detection, script scanning, and traceroute (aggressive)
+#  -O - enable OS detection
+#  -sA - TCP ACK scan
+#  -F - fast scan
+#  --script=vulscan - also access vulnerabilities in target
+
+alias nmap_open_ports="nmap --open"
+alias nmap_list_interfaces="nmap --iflist"
+alias nmap_slow="nmap -sS -v -T1"
+alias nmap_fin="nmap -sF -v"
+alias nmap_full="nmap -sS -T4 -PE -PP -PS80,443 -PY -g 53 -A -p1-65535 -v"
+alias nmap_check_for_firewall="nmap -sA -p1-65535 -v -T4"
+alias nmap_ping_through_firewall="nmap -PS -PA"
+alias nmap_fast="nmap -F -T5 --version-light --top-ports 300"
+alias nmap_detect_versions="nmap -sV -p1-65535 -O --osscan-guess -T4 -Pn"
+alias nmap_check_for_vulns="nmap --script=vulscan"
+alias nmap_full_udp="nmap -sS -sU -T4 -A -v -PE -PS22,25,80 -PA21,23,80,443,3389 "
+alias nmap_traceroute="nmap -sP -PE -PS22,25,80 -PA21,23,80,3389 -PU -PO --traceroute "
+alias nmap_full_with_scripts="sudo nmap -sS -sU -T4 -A -v -PE -PP -PS21,22,23,25,80,113,31339 -PA80,113,443,10042 -PO --script all "
+alias nmap_web_safe_osscan="sudo nmap -p 80,443 -O -v --osscan-guess --fuzzy "
+
+
+
 #-----------
 # Memory:
 #-----------
@@ -448,6 +480,10 @@ allStrings () { pyg "$1" | tr -d "\0" | strings ; }
 # to convert text to HTML and vice vera, use 'textutil'
 # to convert a man page to PDF: man -t foo > foo.ps; open foo.ps; save as PDF
 
+# join strings
+sjoin () { (IFS=$1; shift; echo "$*"); }
+alias perljoin="perl -E 'say join(shift, @ARGV)'"
+
 #------------
 # Processes:
 #------------
@@ -495,9 +531,18 @@ alias gerp='grep'
 alias sl='ls'
 alias hgrp='hgrep'
 
-#------------------------------
+#-----
+# LOL:
+#-----
+
+alias wtf='dmesg'
+alias nomnom='killall'
+alias tldr='less'
+alias ohknowz='cat /var/log/errors.log'
+
+#------
 # TMUX:
-#------------------------------
+#------
 
 alias ta='tmux attach'
 alias tls='tmux ls'
---
+++ common/web-search.sh
@@ -0,0 +1,59 @@
+# web_search from terminal
+
+function web_search() {
+  local site=$1
+  shift
+  # join arguments passed with '+', then append to search engine URL
+  local siteSearchParams=$(sjoin + $@)
+
+  # define search engine URLS
+  declare -A urls
+  urls[google]="https://www.google.com/search?q="
+  urls[bing]="https://www.bing.com/search?q="
+  urls[yahoo]="https://search.yahoo.com/search?p="
+  urls[duckduckgo]="https://www.duckduckgo.com/?q="
+  urls[startpage]="https://www.startpage.com/do/search?q="
+  urls[yandex]="https://yandex.ru/yandsearch?text="
+  urls[github]="https://github.com/search?q="
+  urls[baidu]="https://www.baidu.com/s?wd="
+  urls[ecosia]="https://www.ecosia.org/search?q="
+  urls[goodreads]="https://www.goodreads.com/search?q="
+  urls[stackoverflow]="https://stackoverflow.com/search?q="
+  urls[wikipedia]="https://en.wikipedia.org/w/index.php?search="
+  urls[dictionary]="https://www.thefreedictionary.com/"
+
+  # check whether the search engine is supported
+  if [ -z "$urls[$site]" ]; then
+    echo "Search engine $site not supported."
+    return 1
+  fi
+
+  # search or go to main page depending on number of arguments passed
+  url="${urls[$site]}$siteSearchParams"
+
+  echo $url
+  open_command "$url"
+}
+
+alias bing='web_search bing'
+alias google='web_search google'
+alias yahoo='web_search yahoo'
+alias ddg='web_search duckduckgo'
+alias sp='web_search startpage'
+alias yandex='web_search yandex'
+alias github='web_search github'
+alias baidu='web_search baidu'
+alias ecosia='web_search ecosia'
+alias goodreads='web_search goodreads'
+alias stackoverflow='web_search stackoverflow'
+alias wikipedia='web_search wikipedia' # technically handled by duckduckgo
+alias dictionary='web_search dictionary'
+
+#add your own !bang searches here
+alias wiki='web_search duckduckgo \!w'
+alias news='web_search duckduckgo \!n'
+alias youtube='web_search duckduckgo \!yt'
+alias map='web_search duckduckgo \!m'
+alias image='web_search duckduckgo \!i'
+alias ducky='web_search duckduckgo \!'
+alias define='web_search dictionary'
