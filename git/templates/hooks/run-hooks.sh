d65e8b78ff7d4d2cbffdb2b02d24f1b2e005db14
Michael P Olson <molson35@ford.com>
2020-05-15 11:17:48 -0400

    Updating my dotfiles with latest round of changes with submodules and lfs

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -4,25 +4,37 @@ EXIT_CODE=0
 
 repo=$( git rev-parse --show-toplevel )
 hook_type=$( basename $0 )
-hooks=~/.dotfiles/git/hooks
+hook_dir=~/.dotfiles/git/hooks
+hooktypes=$(find $hook_dir -name "*.$hook_type")
 
 echo "Executing $hook_type hook(s)"
-
-if [ -n "$(git submodule | awk '{ print $2 }')" ]; then
-	git submodule update --init --recursive
-fi
-
-hooktypes=$(find $hooks -name "*.$hook_type")
 for hook in $hooktypes; do
 	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing $hook$COLOR_NONE"
+	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
+	
+	if [ -n "$(git submodule | awk '{ print $2 }')" -a -e "$repo/.gitmodules" ]; then
+		for hk in "post-checkout post-merge"; do
+			git submodule update --init --recursive
+		done
+	fi
+	
+	if command -v git-lfs >/dev/null 2>&1 && [ -e "$repo/.lfsconfig" ]; then
+		for hk in "pre-push post-checkout post-commit post-merge"; do
+			if [ $hook_type=$hk ]; then
+				git lfs $hook_type "$@"
+				echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-merge.\n"
+				exit 2 
+			fi
+		done
+	fi 
+
 	$hook
 	EXIT_CODE=$(($EXIT_CODE + $?))
 done
 
 if [ $EXIT_CODE -ne 0 ]; then
 	echo ""
-	echo "${COLOR_RED}Commit Failed.$COLOR_NONE"
+	echo "${COLOR_RED}${hook_type} Failed.$COLOR_NONE"
 fi
 
 exit $(($EXIT_CODE))
