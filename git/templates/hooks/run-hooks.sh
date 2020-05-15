da074f8ad532e26bc878dfdfc0be8cd085b330ca
Michael P Olson <molson35@ford.com>
2020-05-15 13:53:51 -0400

    Updating my dotfiles with latest round of changes with submodules and lfs

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -2,41 +2,49 @@
 
 EXIT_CODE=0
 
+ps1_bold=$(tput bold)
+ps1_red=$(tput setaf 1)
+ps1_yellow=$(tput setaf 3)
+ps1_magenta=$(tput setaf 5)
+ps1_cyan=$(tput setaf 6)
+ps1_normal=$(tput sgr0)
+
 repo=$( git rev-parse --show-toplevel )
 hook_type=$( basename $0 )
 hook_dir=~/.dotfiles/git/hooks
-hooktypes=$(find $hook_dir -name "*.$hook_type")
+recent_hooks=$(find $hook_dir -name "recent.$hook_type")
+submodule_hooks=$(find $hook_dir -name "submodule.$hook_type")
+lfs_hooks=$(find $hook_dir -name "lfs.$hook_type")
 
 echo "Executing $hook_type hook(s)"
-for hook in $hooktypes; do
+for recent_hook in $recent_hooks; do
 	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
-	
-	if [ -e "$repo/.gitmodules" && -n "$(git submodule | awk '{ print $2 }')" ]; then
-		for hk in "post-checkout post-merge"; do
-			if [ $hook_type=$hk ]; then
-				git submodule sync --recursive
-				git submodule update --init --recursive
-			fi
-		done
-	fi
-	
-	if [ -e "$repo/.lfsconfig" && command -v git-lfs >/dev/null 2>&1 ]; then
-		for hk in "pre-push post-checkout post-commit post-merge"; do
-			if [ $hook_type=$hk ]; then
-				git lfs $hook_type "$@"	echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/post-merge.\n"
-				exit 2 
-			fi
-		done
-	fi 
-
-	$hook
+	echo "${ps1_magenta}Executing ${recent_hook}${ps1_normal}"
+	$recent_hook
 	EXIT_CODE=$(($EXIT_CODE + $?))
 done
 
+if [ -e "$repo/.gitmodules" ]; then
+	for submodule_hook in $submodule_hooks; do
+		echo ""
+		echo "${ps1_magenta}Executing ${submodule_hook}${ps1_normal}"
+		$submodule_hook
+		EXIT_CODE=$(($EXIT_CODE + $?))
+	done
+fi
+
+if [ -e "$repo/.lfsconfig" ]; then
+	for lfs_hook in $lfs_hooks; do
+		echo ""
+		echo "${ps1_magenta}Executing ${lfs_hook}${ps1_normal}"
+		$lfs_hook
+		EXIT_CODE=$(($EXIT_CODE + $?))
+	done
+fi
+
 if [ $EXIT_CODE -ne 0 ]; then
 	echo ""
-	echo "${COLOR_RED}${hook_type} Failed.$COLOR_NONE"
+	echo "${ps1_red}${hook_type} Failed.$ps1_normal"
 fi
 
 exit $(($EXIT_CODE))
